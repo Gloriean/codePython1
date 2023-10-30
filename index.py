2c8a17f939e9924e6df64ac9c7a5c2b5df7f424a
glory <gloryolasanmi@gmail.com>
2023-10-30 11:45:59 +0100

    arrMode

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
-var = ["automation", "teller", "machine"]
-for x in var:
-    print(x)
+#Array module
+import array
 
-var.append("maize")
-print(var)
+obj = array.array('i', [1, 2, 3])
+
+print (type(obj), obj)
+
+var = array.array('u', "BAT")
+print (type(var), var)
