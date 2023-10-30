b90883bf9fd2e746e97ca140d47c2fc1a34e813f
glory <gloryolasanmi@gmail.com>
2023-10-30 15:03:02 +0100

    arrayModule

--- index.py
+++ index.py
@@ -1,9 +1,27 @@
 #Array module
 import array
 
-obj = array.array('i', [1, 2, 3])
+obj = array.array('d', [1.2, 2.9, 3.7])
+print(obj)
+print(type(obj), obj)
+print(obj.buffer_info())
 
-print (type(obj), obj)
+var = array.array('d', [2.2, 4.5, 6.77])
+print(var)
+print(type(var), var)
 
-var = array.array('u', "BAT")
-print (type(var), var)
+num = array.array('I', [4, 5, 6])
+print(num)
+print(type(num), num)
+
+add = array.array('d')
+add = obj + var
+print("Array add = " , add)
+
+
+nM = array.array('i', [6, 67, 7, -9, -2, -12, 5])
+for i in nM:
+    print(i)
+
+for pnt in range(5):
+    print(nM[pnt])
